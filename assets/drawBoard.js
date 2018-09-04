e2723c575374e93d703006215643898d3f09719f
Connor Pawar <connorpawar@gmail.com>
2018-09-04 13:24:07 -0500

    Added comments to drawBoard function

--- assets/drawBoard.js
+++ assets/drawBoard.js
@@ -1,7 +1,7 @@
 function drawBoard(dimension){
      let lastClicked;
-     let grid = clickableGrid(dimension,dimension,function(el,row,col,i){
-       el.className='clicked';
+     let grid = clickableGrid(dimension,dimension,function(element,row,col,i){
+       element.className = 'clicked';
      });
 
      document.body.appendChild(grid);
@@ -11,12 +11,15 @@ function drawBoard(dimension){
        let grid = document.createElement('table');
        grid.className = 'grid';
        for (let r=0; r<rows; r++){
-         let tr = grid.appendChild(document.createElement('tr'));
+         let tr = grid.appendChild(document.createElement('tr')); //creates a new row for each r value
          for (let c=0; c<cols; c++){
-           let cell = tr.appendChild(document.createElement('td'));
-           cell.addEventListener('click',(function(el,r,c,i){
-             return function(){ callback(el,r,c,i); }
-            })(cell,r,c,i),false);
+           let cell = tr.appendChild(document.createElement('td')); //creates a new table data cell in the current row for each column
+           cell.addEventListener('click',(function(element,r,c,i){ //on a click it creates a function scope for all of the local variables for a cell
+             return function(){
+               callback(el,r,c,i); //function that allows refernce to specific instance by creating closure
+             }
+            })(cell,r,c,i),
+            false);
          }
        }
        return grid;
