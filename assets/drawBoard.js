561f2a2379b52ec3c06435cca2941a42e8dd527e
Connor Pawar <connorpawar@gmail.com>
2018-09-04 14:39:35 -0500

    created super simplistic bomb system that should be updated later

--- assets/drawBoard.js
+++ assets/drawBoard.js
@@ -1,7 +1,10 @@
-function drawBoard(dimension){
+function drawBoard(dimension, bombsLeft){
      let lastClicked;
-     let grid = clickableGrid(dimension,dimension,function(element,row,col,i){
+     let grid = clickableGrid(dimension,dimension,function(element,row,col,i,isBomb){
        element.className = 'clicked';
+       if(isBomb == true){
+         element.className = 'bomb';
+       }
      });
 
      document.body.appendChild(grid);
@@ -14,14 +17,30 @@ function drawBoard(dimension){
          let tr = grid.appendChild(document.createElement('tr')); //creates a new row for each r value
          for (let c=0; c<cols; c++){
            let cell = tr.appendChild(document.createElement('td')); //creates a new table data cell in the current row for each column
-           cell.addEventListener('click',(function(element,r,c,i){ //on a click it creates a function scope for all of the local variables for a cell
+           let isBomb = placeBombs(r,c,bombsLeft);
+           if(isBomb == true){
+              bombsLeft--;
+           }
+           cell.addEventListener('click',(function(element,r,c,i,isBomb){ //on a click it creates a function scope for all of the local variables for a cell
              return function(){
-               callback(element,r,c,i); //function that allows refernce to specific instance by creating closure
+               callback(element,r,c,i,isBomb); //function that allows refernce to specific instance by creating closure
              }
-            })(cell,r,c,i),
+           })(cell,r,c,i,isBomb),
             false);
          }
        }
        return grid;
+     }
+
+     function placeBombs(rows, cols, bombsLeft){
+       if(bombsLeft > 0){
+        let check = Math.floor(Math.random() * 10);
+        if (check > 5){
+          return true;
+        }
+        else{
+          return false;
+        }
+      }
      }
    }
