6233b6f88835d3354eb732e2262e716c54757817
Connor Pawar <connorpawar@gmail.com>
2018-09-03 21:14:07 -0500

    created the drawboard method which allows for a clickable grid to be generated by the program

---
+++ assets/drawBoard.js
@@ -0,0 +1,27 @@
+function drawBoard(dimension){
+     let lastClicked;
+     let grid = clickableGrid(dimension,dimension,function(el,row,col,i){
+
+       el.className='clicked';
+       if (lastClicked) lastClicked.className='';
+       lastClicked = el;
+     });
+
+     document.body.appendChild(grid);
+
+     function clickableGrid( rows, cols, callback ){
+       let i=0;
+       let grid = document.createElement('table');
+       grid.className = 'grid';
+       for (let r=0; r<rows; r++){
+         let tr = grid.appendChild(document.createElement('tr'));
+         for (let c=0; c<cols; c++){
+           let cell = tr.appendChild(document.createElement('td'));
+           cell.addEventListener('click',(function(el,r,c,i){
+             return function(){ callback(el,r,c,i); }
+            })(cell,r,c,i),false);
+         }
+       }
+       return grid;
+     }
+   }
