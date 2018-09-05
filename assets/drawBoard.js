8b0512108f22b7e33834d7ae4752b0c3e64c5fde
Connor Pawar <connorpawar@gmail.com>
2018-09-05 18:49:42 -0500

    added a class.js file and made a 2d array with mine locations

--- assets/drawBoard.js
+++ assets/drawBoard.js
@@ -1,44 +1,47 @@
 function drawBoard(dimension, bombsLeft){
      let lastClicked;
+     let field = new mineField(dimension, dimension);
+
      let cellsLeft = dimension * dimension;
 
      let grid = clickableGrid(dimension,dimension,function(element,row,col,i,isBomb){
-       element.className = 'clicked';
-       if(element.isBomb == true){
-         element.className = 'bomb';
+         element.className = 'clicked';
+         if(element.isBomb == true){
+             element.className = 'bomb';
        }
      });
 
      document.body.appendChild(grid);
 
      function clickableGrid( rows, cols, callback ){
-       let i=0;
-       let grid = document.createElement('table');
-       grid.className = 'grid';
-       for (let r=0; r<rows; r++){
-         let tr = grid.appendChild(document.createElement('tr')); //creates a new row for each r value
-         for (let c=0; c<cols; c++){
-           let cell = tr.appendChild(document.createElement('td')); //creates a new table data cell in the current row for each column
-           cell.isBomb = placeBombs(r,c,bombsLeft);
-           cellsLeft--;
-           if(cell.isBomb == true){
-              bombsLeft--;
-           }
-           cell.addEventListener('click',(function(element,r,c,i){ //on a click it creates a function scope for all of the local variables for a cell
-             return function(){
-               callback(element,r,c,i); //function that allows refernce to specific instance by creating closure
+         let i=0;
+         let grid = document.createElement('table');
+         grid.className = 'grid';
+         for (let r=0; r<rows; r++){
+             let tr = grid.appendChild(document.createElement('tr')); //creates a new row for each r value
+             for (let c=0; c<cols; c++){
+                 let cell = tr.appendChild(document.createElement('td')); //creates a new table data cell in the current row for each column
+                 cell.isBomb = placeBombs(r,c,bombsLeft);
+                 cellsLeft--;
+                 if(cell.isBomb == true){
+                     bombsLeft--;
+                     field.arr[r][c] = true;
+                 }
+                 cell.addEventListener('click',(function(element,r,c,i){ //on a click it creates a function scope for all of the local variables for a cell
+                     return function(){
+                         callback(element,r,c,i); //function that allows refernce to specific instance by creating closure
+                     }
+                 })(cell,r,c,i),false);
              }
-           })(cell,r,c,i),
-            false);
          }
-       }
-       return grid;
+         return grid;
      }
-
      function placeBombs(rows, cols, bombsLeft){
-       if(bombsLeft > 0){
-        return(Math.random() <= (bombsLeft / cellsLeft));
-      }
-      return false;
-     }
+         if(bombsLeft > 0){
+             return(Math.random() <= (bombsLeft / cellsLeft));
+         }
+         return false;
+    }
+
+    console.log(field.arr);
 }
