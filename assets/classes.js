0ad49c838c0a56e73566477cb62b7830c6a01930
kylelindteigen <kylelindteigen@ku.edu>
2018-09-05 23:11:29 -0500

    Parital implementation of the expanding field also can't get bomb placement working

--- assets/classes.js
+++ assets/classes.js
@@ -3,34 +3,71 @@ class Tile {
 		this.mine = false;
 		this.flag = false;
 		this.adjminenum = 0;
+		this.cell = 0;
 	}
 }
 class mineField {
     constructor(height, width, numBombs){
         this.height = height;
         this.width = width;
+		this.bombs = numBombs;
+		this.flags = numBombs;
         this.arr = [];
 		let cells = height * width;
 		let x = new Tile();
         for(let i = 0; i < width; i++){
-            this.arr.push([x]);
+            this.arr.push([0]);
             for(let j = 0; j < height; j++){
                 this.arr[i][j] = x;
-				/*if(numBombs > 0){
-	                this.arr[i][j].mine = Math.random() <= (numBombs / cells));
-					numBombs --;
-	            }
-				cells --;*/
             }
         }
-		while(numBombs > 0){
-			for(let i = 0; i < width; i++){
-	            for(let j = 0; j < height; j++){
-					this.arr[i][j].mine = false;
-					numBombs --;
-	            }
-	        }
-		}
     }
-	//console.log(numBombs);
+	Click(element,row,col,i,dimension){
+		if(element.field.arr[row][col].adjminenum == 0 && element.field.arr[row][col].mine != true){
+			element.field.Expand(element,row,col,dimension);
+		}
+        else if(element.field.arr[row][col].mine == true){
+        	element.className = 'bomb';
+		}
+	}
+	Flag(x,y) {
+		this.arr[x][y].flag = true;
+		flags --;
+		if(flags == 0) {
+
+		}
+	}
+	Expand(element, row, col, dimension) {
+		if(element.className == 'clicked'){
+			console.log("you did it");
+			return;
+		}
+		if(element.field.arr[row][col].adjminenum == 0){
+			element.className = 'clicked';
+			if(row > 0){
+				element.field.Expand(this.arr[row-1][col].cell,row-1,col,dimension);
+				if(col > 0){
+					element.field.Expand(this.arr[row-1][col-1].cell,row-1,col-1,dimension);
+				}
+				if(col > dimension-1){
+					element.field.Expand(this.arr[row-1][col+1].cell,row-1,col+1,dimension);
+				}
+			}
+			if(col > 0){
+				element.field.Expand(this.arr[row][col-1].cell,row,col-1,dimension);
+			}
+			if(col < dimension-1){
+				element.field.Expand(this.arr[row][col+1].cell,row,col+1,dimension);
+			}
+			if(row < dimension-1){
+				element.field.Expand(this.arr[row+1][col].cell,row + 1,col,dimension);
+				if(col > 0){
+					element.field.Expand(this.arr[row+1][col-1].cell,row+1,col-1,dimension);
+				}
+				if(col > dimension-1){
+					element.field.Expand(this.arr[row+1][col+1].cell,row+1,col+1,dimension);
+				}
+			}
+		}
+	}
 }
--- assets/drawBoard.js
+++ assets/drawBoard.js
@@ -5,36 +5,7 @@ function drawBoard(dimension, bombsLeft){
     let cellsLeft = dimension * dimension;
 
     let grid = clickableGrid(dimension,dimension,function(element,row,col,i){
-		if(element.tile.adjminenum == 0){
-			element.className = 'clicked';
-		}
-		else if(element.tile.adjminenum == 1){
-			element.className = 'clicked1';
-		}
-		else if(element.tile.adjminenum == 2){
-			element.className = 'clicked2';
-		}
-		else if(element.tile.adjminenum == 3){
-			element.className = 'clicked3';
-		}
-		else if(element.tile.adjminenum == 4){
-			element.className = 'clicked4';
-		}
-		else if(element.tile.adjminenum == 5){
-			element.className = 'clicked5';
-		}
-		else if(element.tile.adjminenum == 6){
-			element.className = 'clicked6';
-		}
-		else if(element.tile.adjminenum == 7){
-			element.className = 'clicked7';
-		}
-		else if(element.tile.adjminenum == 8){
-			element.className = 'clicked8';
-		}
-        else if(element.tile.mine == true){
-        	element.className = 'bomb';
-		}
+		element.field.Click(element,row,col,i,dimension);
 	});
 
     document.body.appendChild(grid);
@@ -47,26 +18,15 @@ function drawBoard(dimension, bombsLeft){
         	let tr = grid.appendChild(document.createElement('tr')); //creates a new row for each r value
             for (let c=0; c<cols; c++){
             	let cell = tr.appendChild(document.createElement('td')); //creates a new table data cell in the current row for each column
-                cell.tile = field.arr[r][c];
-                /*cellsLeft--;
-                if(cell.isBomb == true){
-                	bombsLeft--;
-                    field.arr[r][c] = true;
-                }*/
+                cell.field = field;
                 cell.addEventListener('click',(function(element,r,c,i){ //on a click it creates a function scope for all of the local variables for a cell
                 	return function(){
                     	callback(element,r,c,i); //function that allows refernce to specific instance by creating closure
                     }
                 })(cell,r,c,i),false);
+				field.arr[r][c].cell = cell;
         	}
         }
         return grid;
 	}
-    function placeBombs(rows, cols, bombsLeft){
-    	if(bombsLeft > 0){
-        	return(Math.random() <= (bombsLeft / cellsLeft));
-        }
-        return false;
-	}
-	console.log(field.arr);
 }
