488c29377f4136252ff6afec60356fb805b28f1b
Devin Suttles <devinfnsuttles@gmail.com>
2018-09-07 22:24:56 -0500

    Resolve merge conflict in drawBoard.js

--- assets/js/drawBoard.js
+++ assets/js/drawBoard.js
@@ -13,41 +13,7 @@ function drawBoard(dimension, bombsLeft){
 		element.field.Click(cell,row,col,i,dimension);
 	});
 
-<<<<<<< HEAD:assets/js/drawBoard.js
-     function clickableGrid( rows, cols, callback ){
-       let i=0;
-       let grid = document.createElement('table');
-       grid.className = 'grid';
-       for (let r=0; r<rows; r++){
-         let tr = grid.appendChild(document.createElement('tr')); //creates a new row for each r value
-         for (let c=0; c<cols; c++){
-           let cell = tr.appendChild(document.createElement('td')); //creates a new table data cell in the current row for each column
-           cell.isBomb = placeBombs(r,c,bombsLeft);
-           cellsLeft--;
-           if(cell.isBomb == true){
-              bombsLeft--;
-           }
-           cell.addEventListener('click',(function(element,r,c,i){ //on a click it creates a function scope for all of the local variables for a cell
-             return function(){
-               callback(element,r,c,i); //function that allows refernce to specific instance by creating closure
-             }
-           })(cell,r,c,i),
-            false);
-         }
-       }
-       return grid;
-     }
-     function placeBombs(rows, cols, bombsLeft){
-       if(bombsLeft > 0){
-        return(Math.random() <= (bombsLeft / cellsLeft));
-      }
-      return false;
-     }
-     function game(let rows,let cols,)
-     {
 
-     }
-=======
     document.body.appendChild(grid);
 
 	function clickableGrid( rows, cols, callback ){
